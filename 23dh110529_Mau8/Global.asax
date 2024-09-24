d4aed29c093e73c6486b4cb1e2b765c4b8507c2a
PC <PC@DESKTOP-RBPO1LM>
2024-09-24 11:51:33 +0700

    Add project files.

---
+++ 23dh110529_Mau8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23dh110529_Mau8.MvcApplication" Language="C#" %>
---
+++ 23dh110529_Mau8/Scripts/JS cua tao/UserJS.js
@@ -0,0 +1,6 @@
+﻿function Today()
+{
+    var date = new Date();
+    var month = date.getMonth() +1;
+    alert("Today is: " + date.getDate() + "Month: " + date.getMonth() + "Year: " + date.getFullYear());
+}
